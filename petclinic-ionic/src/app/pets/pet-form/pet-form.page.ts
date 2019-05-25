e6bfd05e5395ef4a8c150cf0cc16456e1046ab23
Alfredo Arias <alfredo@xiobit.com>
2019-05-24 21:15:51 -0400

    llamada a camera api

--- petclinic-ionic/src/app/pets/pet-form/pet-form.page.ts
+++ petclinic-ionic/src/app/pets/pet-form/pet-form.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Kind} from '../../model/kind.enum';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {CameraResultType, CameraSource, Plugins} from '@capacitor/core';
 
 @Component({
   selector: 'app-pet-form',
@@ -8,11 +10,25 @@ import {Kind} from '../../model/kind.enum';
 })
 export class PetFormPage implements OnInit {
 
+  photoUrl: SafeResourceUrl;
   kinds = Kind;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+
+  async takePhoto() {
+    console.log('Take Pet\'s photo');
+
+    const image = await Plugins.Camera.getPhoto({
+        quality: 85,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+    });
+
+    this.photoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
   }
 
 }
