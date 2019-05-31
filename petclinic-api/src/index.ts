dd64b31523b767b55cbd4823731ab57daf8a401c
Alfredo Arias <alfredo@xiobit.com>
2019-05-31 17:00:48 -0400

    upload images

--- petclinic-api/src/index.ts
+++ petclinic-api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fileUpload, {UploadedFile} from "express-fileupload";
 import * as bodyParser from "body-parser";
 import * as petController from "./controllers/petController"
 
@@ -18,6 +19,12 @@ app.use((req, res, next) => {
 // Usamos la libreria body-parser para poder usar json en el body de los POST
 app.use(bodyParser.json());
 
+// Usamos FileUpload
+app.use(fileUpload());
+
+// Static content
+app.use(express.static("public"));
+
 // definir la ruta principal para nuestra web page
 app.get( "/", ( req, res ) => {
     res.send( "Bienvenido al API de PetClinic 1.0");
@@ -29,6 +36,27 @@ app.get("/pet/:id", petController.getPet);
 app.post("/pet", petController.addPet);
 app.put("/pet/:id", petController.updatePet);
 app.delete("/pet/:id", petController.deletePet);
+
+app.post( "/upload/:id", ( req, res ) => {
+    const petId = req.params.id;
+
+    if (Object.keys(req.files).length === 0) {
+        return res.status(400).send("No files were uploaded.");
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    const sampleFile = req.files.sampleFile;
+
+    console.log(sampleFile);
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv("public/" + petId + ".jpg", (err: any) => {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send("File uploaded!");
+    });
+} );
 
 // Inicializamos el server de Express
 app.listen( port, () => {
